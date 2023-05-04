2496ec229aaf7838d4f506f73b360bbe44b2d590
Allan Xavier <131976799+CoderAllan2013@users.noreply.github.com>
2023-05-04 21:04:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function function1() {
+    document.getElementById("audio1").play();
+  }
+
+  function function2() {
+    document.getElementById("audio2").play();
+  }
+
+  function function4() {
+    document.getElementById("audio4").play();
+  }
+
+  function function5() {
+    document.getElementById("audio5").play();
+  }
+  
+  function function10() {
+    document.getElementById("audio6").play();
+  }
+  
